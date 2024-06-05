1a49d264ef5401fe59b9e8a67a959af9d5d381cf
Nida Firdaws <nida.firdaws03@gmail.com>
2024-06-04 23:08:14 -0700

    Renamed files and made minor changes

---
+++ src/Components/Chlorodynamictext.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<section>
+  <p class="body-text">
+     Peak alien sighting time seems to be between 11PM and 7AM, possibly when 
+     random objects might seem more blurry and alien-like against the dark night sky... We can see that 
+     any time is a good time to see a UFO on the West Coast, with California taking top UFO sightings for 
+     every hour of the day. Perhaps there are more UFO enthusiasts living in the Sunshine state. 
+     We notice another trend where Washington slowly increases its number of sightings as the day progresses from 12AM 
+     to 11PM. Are the Aliens on a schedule? Who knows! We observe that the numbers
+     stay relatively consistent in the midwest and the east coast, with more drastic changes on the west. We presume this is due to the size of the states, maybe also due to more flying activity in general with major cities.
+  </p>
+  <br>
+  <p class="body-text">
+    
+  </p>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+</section>
+
+<style>
+  .body-text{
+    color: white;
+  }
+</style>
--- src/Components/Conclusion.svelte
+++ src/Components/Conclusion.svelte
@@ -1,5 +1,5 @@
 <section id="conclusion">
-  <h1 class="body-header">Our Reccomendations</h1>
+  <h1 class="body-header">Our Recommendations</h1>
   <p class="body-text">
     Based off of the data, we have selected prime locations,times, and shapes to help you 
     get the best chance of spotting a real UFO. 
@@ -8,7 +8,7 @@
     <b>States: Kentucky, Michigan, Indiana, Ohio</b>
     <br>
     <br>
-    <b>Times:    &#123;insert times here&#125;</b>
+    <b>Times: 9PM in Kentucky, 10PM in Michigan, 9PM in Ohio </b>
     <br>
     <br>
     <b>Shapes: Circular Fireball</b>
@@ -18,7 +18,7 @@
     being completely dominated by airports and military bases. It's to no surprise the majority of the sightings happend in the afternoon or late at night 
     as that would provide the best visibility of a shining object in the sky. Lastly we looked and examined the most common 
     shapes people have accounted and looked more into the context where we found that a circular fireball shape is the most
-    likely to be something that isn't easily explainable.
+    likely to be something that isn't easily explainable.Most of the sightings in the top three states happen between 9 and 10PM, with over 399 sightings in Ohio at 10PM. 
     <br>
     <br>
     If your are able to make a trip for the best chance at UFO hunting, we hope you keep our reccomendations in mind. If not, 
